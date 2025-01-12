de9ee585e16a8a1abb72511ba02579b6f7ec00a2
touseefiqbal845 <touseefiqbal845@gmail.com>
2025-01-13 01:09:15 +0500

    server

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const app = require('./app');
+// require('dotenv').config();
+const express = require('express');
+const connectDB = require('./src/config/database');
 
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Database Connection
+connectDB();
+
+// Test Route
+app.get('/', (req, res) => {
+  res.status(200).send('API is running successfully');
+});
+
+// Start the Server
 const PORT =  5000;
-
-
-app.get("/", (req, res) => {
-  console.log("Success");
-  res.send("Hello, World!"); 
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} `);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
