01111aaa55b1384d2d17a852a79fd2e1c0ab9dda
touseefiqbal845 <touseefiqbal845@gmail.com>
2025-01-13 01:36:54 +0500

    chn

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ app.use(express.json());
 
 // Database Connection
 
-const connectDB = () => {
-    console.log('Attempting to connect to MongoDB...');
-  mongoose
-    .connect("mongodb+srv://touseefiqbal845:Punjabuni321@easypickercluster.pzxaxac.mongodb.net/?retryWrites=true&w=majority&appName=easyPickerCluster")
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch((err) => {
-      console.error(`MongoDB connection error: ${err.message}`);
-      process.exit(1);
-    });
-};
-connectDB()
-
 // Test Route
 app.get('/', (req, res) => {
   res.status(200).send('API is running successfully');
 });
+
+const connectDB = () => {
+  console.log('Attempting to connect to MongoDB...');
+mongoose
+  .connect("mongodb+srv://touseefiqbal845:Punjabuni321@easypickercluster.pzxaxac.mongodb.net/?retryWrites=true&w=majority&appName=easyPickerCluster")
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => {
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
+  });
+};
+connectDB()
 
 // Start the Server
 const PORT =  5000;
