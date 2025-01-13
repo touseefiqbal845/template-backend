962c1d8fea8e0e0f9a3d55e86c9f8e213b097c94
touseefiqbal845 <touseefiqbal845@gmail.com>
2025-01-13 14:55:05 +0500

    builddel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
 
 
 // Start the server
-const PORT = process.env.PORT || "https://tasky-backend-eosin.vercel.app/";
+const PORT =  "https://template-backend-beige.vercel.app/";
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
